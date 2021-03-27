8157dd279056e3ae6649b82054e26e9fd963e088
mayaemerson <emersonmaia@me.com>
2021-03-27 11:38:11 -0300

    organizando como chamar as planilhas

--- src/onedit.js
+++ src/onedit.js
@@ -20,37 +20,42 @@ a data e hora se precisar pode formatar exemplos na pasta
 formatdatehora (os exemplos mostrado são para o horário Brasileiro)
 */
 function carimboDateHor(){
-
-  abExtra.getRange("C2").setFormula('=TODAY()')
-    const lra0 = abExtra.getLastRow()
-    const ftlinha = abExtra.getRange(2,3,lra0-1)
+// extras é a aba da planilha
+const SheetContacts = SpreadsheetApp.getActiveSpreadsheet()
+const ss =  SheetContacts.getSheetByName("extras");
+  ss.getRange("C2").setFormula('=TODAY()')
+    const lra0 = ss.getLastRow()
+    const ftlinha = ss.getRange(2,3,lra0-1)
    
-   abExtra.getRange("C2").copyTo(ftlinha)
-
-
+   ss.getRange("C2").copyTo(ftlinha)
 }
 
 
 //exemplo data vencimento
 function dataVencimento() {
-  
+  // extras é a aba da planilha
+const SheetContacts = SpreadsheetApp.getActiveSpreadsheet()
+const ss =  SheetContacts.getSheetByName("extras");
 
-  abExtra.getRange("D2").setFormula('=IF(B3="";"";IF(C3-TODAY()>30;"Prazo - Mais de 1 mês"; IF(C3=TODAY();"Hoje";  IF(C3<TODAY();"Venceu"; IF(C3-TODAY()<10;"Prazo - Menos de 10 dias"; IF(C3-TODAY()<15;"Prazo - Menos de 15 dias"; "Prazo - Menos de 1 Mês"))))))')
-    const lra1 = abExtra.getLastRow()
-    const ftlinha = abExtra.getRange(2,4,lra1-1)
+  ss.getRange("D2").setFormula('=IF(B3="";"";IF(C3-TODAY()>30;"Prazo - Mais de 1 mês"; IF(C3=TODAY();"Hoje";  IF(C3<TODAY();"Venceu"; IF(C3-TODAY()<10;"Prazo - Menos de 10 dias"; IF(C3-TODAY()<15;"Prazo - Menos de 15 dias"; "Prazo - Menos de 1 Mês"))))))')
+    const lra1 = ss.getLastRow()
+    const ftlinha = ss.getRange(2,4,lra1-1)
    
-   abExtra.getRange("D2").copyTo(ftlinha)
+   ss.getRange("D2").copyTo(ftlinha)
 
 }
 
 //exempo numeração automatica 
 
 function numerarId(){
+   // extras é a aba da planilha
+const SheetContacts = SpreadsheetApp.getActiveSpreadsheet()
+const ss =  SheetContacts.getSheetByName("extras");
    const id = "1"
-  abExtra.getRange("A2").setValue(id)
-  abExtra.getRange("A3").setFormula('=OFFSET(A3;-1;0)+1')
-    const lra2 = abExtra.getLastRow()
-    const ftlinha = abExtra.getRange(3,1,lra2-1)
+  ss.getRange("A2").setValue(id)
+  ss.getRange("A3").setFormula('=OFFSET(A3;-1;0)+1')
+    const lra2 = ss.getLastRow()
+    const ftlinha = ss.getRange(3,1,lra2-1)
    
-   abExtra.getRange("A3").copyTo(ftlinha)
+   ss.getRange("A3").copyTo(ftlinha)
 }
